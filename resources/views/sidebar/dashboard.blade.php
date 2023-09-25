819540860382a603bcf530963f665b5f6e5869b2
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-09-25 11:20:20 +0700

    Update dashboard.blade.php

--- resources/views/sidebar/dashboard.blade.php
+++ resources/views/sidebar/dashboard.blade.php
@@ -3,9 +3,12 @@
 @section('content')
     <div class="flex">
         <x-sidebar />
-        <x-navigation />
-
-        <div>
+        <div class="w-full">
+            <x-navigation />
+            
+            <div class="mt-5 ml-5">
+                <h1 class="text-2xl">DASHBOARD</h1>
+            </div>
         </div>
     </div>
 @endsection
