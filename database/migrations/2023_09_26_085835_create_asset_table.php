015967ae297aaac1fbaf524c470fd1abb6ea9d00
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-09-26 21:22:15 +0700

    Create 2023_09_26_085835_create_asset_table.php

---
+++ database/migrations/2023_09_26_085835_create_asset_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('asset', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang');
+            $table->decimal('harga_barang', 10, 2);
+            $table->string('nomor_seri_barang')->unique();
+            $table->integer('jumlah_barang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('asset');
+    }
+};
