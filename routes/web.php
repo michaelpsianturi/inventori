58ce286ef68560cbfe333ba25930152ec498cfd5
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-09-29 19:45:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserlistController;
-use App\Http\Controllers\AssetController;
 use App\Http\Controllers\AccessoriesController;
-
+use App\Livewire\AssetList;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,5 +12,5 @@ Route::get('/', function () {
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/listuser', [UserlistController::class, 'index']);
-Route::get('/asset', [AssetController::class, 'show']);
+Route::get('/asset', AssetList::class);
 Route::get('/accessories', [AccessoriesController::class, 'show']);
