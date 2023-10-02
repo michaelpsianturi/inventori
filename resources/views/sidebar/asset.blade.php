5c5c2dd901737aa2a511dfebc13451102556f737
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-02 18:02:36 +0700

    Update asset.blade.php

--- resources/views/sidebar/asset.blade.php
+++ resources/views/sidebar/asset.blade.php
@@ -13,18 +13,31 @@
                     </svg>
                     Insert Data
                 </button>
+                <div class="mt-4">
+                    <table class="w-full ml-4 bg-white divide-y rounded-lg table-auto max-w-7xl">
+                        <thead>
+                          <tr class="grid grid-cols-4">
+                            <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Nama Barang</th>
+                            <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Harga</th>
+                            <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Nomor Seri</th>
+                            <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Jumlah</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($assets as $asset)
+                            <tr class="grid grid-cols-4 divide-y">
+                              <td class="py-4 pl-4 pr-3 text-sm font-medium whitespace-nowrap text-gray-90">{{ $asset->nama_barang }}</td>
+                              <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $asset->harga_barang }}</td>
+                              <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $asset->nomor_seri_barang }}</td>
+                              <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $asset->jumlah_barang }}</td>
+                            </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                      
+                </div>
             </div>
         </div>
     </div>
 
-    <div>
-        @foreach ($assets as $asset)
-            <tr>
-                <td>{{ $asset->nama_barang }}</td>
-                <td>{{ $asset->harga_barang }}</td>
-                <td>{{ $asset->nomor_seri_barang }}</td>
-                <td>{{ $asset->jumlah_barang }}</td>
-            </tr>
-        @endforeach
-    </div>
 @endsection
