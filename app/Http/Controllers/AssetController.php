604300c93acf10416c8b3298fd53980983800832
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-02 18:02:30 +0700

    Create AssetController.php

---
+++ app/Http/Controllers/AssetController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Asset;
+
+class AssetController extends Controller
+{
+    public $assets;
+
+    public function mount()
+    {
+        $this->assets = Asset::all();
+
+    }
+
+    public function render()
+    {
+        $assets = Asset::all();
+        return view('sidebar.asset', [
+            'assets' => $assets,
+        ]);
+    }
+}
+
