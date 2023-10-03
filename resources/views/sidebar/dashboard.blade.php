394bd3c2a0b731336329f25c3a2984740c2c28c1
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-03 13:59:02 +0700

    Update dashboard.blade.php

--- resources/views/sidebar/dashboard.blade.php
+++ resources/views/sidebar/dashboard.blade.php
@@ -16,7 +16,7 @@
                                 <h2 class="py-6 bg-white">3</h2>
                                 <h3 class="flex p-1 text-left bg-slate-50">
                                     TABEL ASSET
-                                    <a href="#" class="ml-4">
+                                    <a href="/asset" class="ml-4">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                                         </svg>
@@ -30,7 +30,7 @@
                                 <h2 class="py-6 bg-white">3</h2>
                                 <h3 class="flex p-1 text-left bg-slate-50">
                                     TABEL USER
-                                    <a href="#" class="ml-4">
+                                    <a href="/listuser" class="ml-4">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                                         </svg>
