2a44c73a03057a9ee387c8073aefe70a778acfcf
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-04 11:12:14 +0700

    Update asset.blade.php

--- resources/views/sidebar/asset.blade.php
+++ resources/views/sidebar/asset.blade.php
@@ -7,14 +7,9 @@
             <x-navigation />
             <div class="ml-5 mt-7">
                 <h1 class="text-2xl">Data Asset</h1>
-                <button class="flex p-1 mt-2 bg-blue-400 rounded-md hover:bg-blue-600 text-slate-800 hover:text-white hover:shadow-md">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                    </svg>
-                    Insert Data
-                </button>
+                <x-alpine.modal />
                 <div class="mt-4">
-                    <table class="w-full ml-4 bg-white divide-y rounded-lg table-auto max-w-7xl">
+                    <table class="w-11/12 ml-4 bg-white divide-y rounded-lg table-auto max-w-7xl">
                         <thead>
                           <tr class="grid grid-cols-4">
                             <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Nama Barang</th>
