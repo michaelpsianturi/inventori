4e918b08cd919603bfc65ff086ece6e1cde81cfd
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-04 11:11:59 +0700

    Update datauser.php

--- app/Models/datauser.php
+++ app/Models/datauser.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class datauser extends Model
 {
     use HasFactory;
+
+    protected $table = 'profile';
+
+    protected $fillable = [
+        'nama_pengguna',
+        'email',
+        'phone_number',
+        'alamat',
+    ];
 }
