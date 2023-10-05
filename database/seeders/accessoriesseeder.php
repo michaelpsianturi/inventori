313efd4dfc7cee01fdff8778aa97949764908e82
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-05 11:42:37 +0700

    updated

---
+++ database/seeders/accessoriesseeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\accessories;
+use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class accessoriesseeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+
+        $accessoriesitem = [
+            'Keyboard',
+            'HDMI Cable',
+            'Stand laptop',
+            'Speaker',
+            'Converter',
+            'VGA Cable',
+            'Power Cable',
+            'SSD',
+            'RAM',
+            'Hardisk',
+        ];
+
+        for ($i = 0; $i < 15; $i++) {
+            accessories::create([
+                'nama_barang' => $faker-> randomElement($accessoriesitem),
+                'harga_barang' => rand(19999,199999 ),
+                'nomor_seri_barang' => $faker-> numberBetween(1000, 9999),
+                'jumlah_barang' => rand(0,20)
+            ]);
+        }
+    }
+}
--- resources/views/sidebar/accessories.blade.php
+++ resources/views/sidebar/accessories.blade.php
@@ -3,9 +3,31 @@
 @section('content')
     <div class="flex">
         <x-sidebar />
-        <x-navigation />
-
-        <div>
+        
+        <div class="w-full">
+            <x-navigation />
+            <div class="mt-6">
+                <table class="w-11/12 ml-4 bg-white divide-y rounded-lg table-auto max-w-7xl">
+                    <thead>
+                      <tr class="grid grid-cols-4">
+                        <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Nama Barang</th>
+                        <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Harga</th>
+                        <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Nomor Seri</th>
+                        <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Jumlah</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($accessories as $accessories)
+                            <tr class="grid grid-cols-4 bg-white divide-y divide-gray-200">
+                                <td class="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6">{{ $accessories->nama_barang }}</td>
+                                <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $accessories->harga_barang }}</td>
+                                <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $accessories->nomor_seri_barang }}</td>
+                                <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $accessories->jumlah_barang }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
