ba482db7c8e562ae3fae203aef73af167a7ba30f
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-06 11:28:51 +0700

    Update modal.blade.php

--- resources/views/components/alpine/modal.blade.php
+++ resources/views/components/alpine/modal.blade.php
@@ -6,7 +6,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
-            Open dialog
+            Add Asset
         </button>
     </span>
 
@@ -36,10 +36,29 @@
                 class="relative w-full max-w-2xl p-12 overflow-y-auto bg-white shadow-lg rounded-xl"
             >
                 <!-- Title -->
-                <h2 class="text-3xl font-bold" :id="$id('modal-title')">Confirm</h2>
+                <h2 class="text-3xl font-bold" :id="$id('modal-title')">Add Asset</h2>
 
                 <!-- Content -->
-                <p class="mt-2 text-gray-600">Are you sure you want to learn how to create an awesome modal?</p>
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Nama Asset</label>
+                    <div class="mt-2">
+                      <input type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2" placeholder="Laptop,PC,Monitor">
+                    </div>
+                </div>
+
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Harga Asset</label>
+                    <div class="mt-2">
+                      <input type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6 pl-2">
+                    </div>
+                </div>
+
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">No Seri</label>
+                    <div class="mt-2">
+                      <input type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2">
+                    </div>
+                </div>
 
                 <!-- Buttons -->
                 <div class="flex mt-8 space-x-2">
