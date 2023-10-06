405cca785195c7d57995470c7c0f6976bc3b24a7
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-06 11:29:05 +0700

    Update listuser.blade.php

--- resources/views/sidebar/listuser.blade.php
+++ resources/views/sidebar/listuser.blade.php
@@ -5,27 +5,30 @@
         <x-sidebar />
         <div class="w-full">
             <x-navigation />
-            
-            <table class="w-11/12 mt-8 ml-12 bg-white divide-y rounded-lg table-auto ">
-                <thead>
-                  <tr class="grid grid-cols-4 gap-2">
-                    <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Nama</th>
-                    <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Email</th>
-                    <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">No.Telp</th>
-                    <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Alamat</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach ($profile as $profile)
-                        <tr class="grid grid-cols-4 bg-white divide-y divide-gray-200">
-                            <td class="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 border-b sm:pl-6">{{ $profile->nama_pengguna }}</td>
-                            <td class="py-4 text-sm text-gray-500 ">{{ $profile->email }}</td>
-                            <td class="py-4 text-sm text-gray-500 ">{{ $profile->phone_number }}</td>
-                            <td class="py-4 pr-6 -ml-20 text-sm text-gray-500 ">{{ $profile->alamat }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="ml-5 mt-7">
+                <h1 class="text-2xl">List User</h1>
+                <x-alpine.modal />
+                <table class="w-11/12 mt-4 ml-4 bg-white divide-y rounded-lg table-auto max-w-7xl">
+                    <thead>
+                      <tr class="grid grid-cols-4 gap-2">
+                        <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Nama</th>
+                        <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Email</th>
+                        <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">No.Telp</th>
+                        <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">Alamat</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($profile as $profile)
+                            <tr class="grid grid-cols-4 bg-white divide-y divide-gray-200">
+                                <td class="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 border-b sm:pl-6">{{ $profile->nama_pengguna }}</td>
+                                <td class="py-4 text-sm text-gray-500 ">{{ $profile->email }}</td>
+                                <td class="py-4 text-sm text-gray-500 ">{{ $profile->phone_number }}</td>
+                                <td class="py-4 pr-6 -ml-20 text-sm text-gray-500 ">{{ $profile->alamat }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
 
     </div>
