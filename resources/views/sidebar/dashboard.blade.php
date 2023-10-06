f27694aac388d90617ce36c48058cacdbbcce347
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-06 11:29:03 +0700

    Update dashboard.blade.php

--- resources/views/sidebar/dashboard.blade.php
+++ resources/views/sidebar/dashboard.blade.php
@@ -44,7 +44,7 @@
                                 <h2 class="py-6 bg-white">3</h2>
                                 <h3 class="flex p-1 text-left bg-slate-50">
                                     TABEL ACCESSORIES
-                                    <a href="#" class="ml-4">
+                                    <a href="/accessories" class="ml-4">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                                         </svg>
