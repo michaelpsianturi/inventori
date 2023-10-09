5935762d8abe6da2c9d581277fb1eecded3bbfee
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-09 11:45:24 +0700

    Update ModalLivewire.php

--- app/Livewire/ModalLivewire.php
+++ app/Livewire/ModalLivewire.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class ModalLivewire extends Component
 {
+
+    public $tittle;
+    public $name;
+    public $email;
+
+    public function mount()
+    {
+        $this -> tittle = $this -> viewData['tittle'];
+    }
+
     public function render()
     {
         return view('livewire.modal-livewire');
