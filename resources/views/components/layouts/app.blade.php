92fedd4000298b59f80c91947e926d3998f222fd
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-10 13:27:05 +0700

    Create app.blade.php

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inventori</title>
+    @vite('resources/css/app.css')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+</head>
+<style>
+    [x-cloack] { display: none !important; }
+</style>
+<body class="pb-4 bg-slate-100">
+    {{ $slot }}
+
+    @livewireScripts
+    <script src="/js/app.js"></script>
+    @stack('scripts')
+</body>
+</html>
