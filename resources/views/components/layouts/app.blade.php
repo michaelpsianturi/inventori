6145e4f7f5d6a8cbd342cd4e1292d1d67217a503
Michaelpsianturi <michaelpsianturi@gmail.com>
2023-10-12 13:49:21 +0700

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <title>Inventori</title>
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @livewireStyles
 
 </head>
 <style>
