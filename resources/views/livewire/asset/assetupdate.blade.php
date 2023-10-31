f2fc7430903e961156939cf4071b7f363df8c61a
Michael p sianturi <michaelpsianturi@gmail.com>
2023-10-31 09:19:02 +0700

    user & asset update

--- resources/views/livewire/asset/assetupdate.blade.php
+++ resources/views/livewire/asset/assetupdate.blade.php
@@ -7,7 +7,7 @@
 
             <x-slot name="content">
                 <div>
-                    <h2 class="mb-2 -mt-3 text-xl ">Nama Barang</h2>
+                    <h2 class="mb-2 -mt-3 text-xl">Nama Barang</h2>
                     <input class="block p-2 w-2/3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text" wire:model="nama_barang">
                     <div>
                         @error('nama_barang') <span class="eror">{{ $message }}</span> @enderror
--- resources/views/livewire/user/userupdate.blade.php
+++ resources/views/livewire/user/userupdate.blade.php
@@ -2,14 +2,41 @@
     <x-baseupdate.layout>
         <div>
             <x-slot name="title">
-                <h2>Update User</h2>
+                <h2 class="text-2xl text-slate-700">Update User</h2>
             </x-slot>
             
             <x-slot name="content">
-                <input class="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text" wire:model="nama_pengguna">
-                <input class="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="email" wire:model="email">
-                <input class="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text" wire:model="phone_number">
-                <input class="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text" wire:model="alamat">
+                <div>
+                    <h2 class="mb-2 -mt-3 text-xl">Nama Pengguna</h2>
+                    <input class="block p-2 w-2/3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text" wire:model="nama_pengguna">
+                    <div>
+                        @error('nama_pengguna') <span class="eror">{{ $message }}</span> @enderror
+                    </div>
+                </div>
+
+                <div>
+                    <h2 class="mt-4 mb-2 text-xl">Email</h2>
+                    <input class="block p-2 w-2/3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="email" wire:model="email">
+                    <div>
+                        @error('email') <span class="eror">{{ $message }}</span> @enderror
+                    </div>
+                </div>
+
+                <div>
+                    <h2 class="mt-4 mb-2 text-xl">No Telp</h2>
+                    <input class="block p-2 w-2/3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text" wire:model="phone_number">
+                    <div>
+                        @error('phone_number') <span class="eror">{{ $message }}</span> @enderror
+                    </div>
+                </div>
+
+                <div>
+                    <h2 class="mt-4 mb-2 text-xl">Alamat</h2>
+                    <input class="block p-2 w-2/3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text" wire:model="alamat">
+                    <div>
+                        @error('alamat') <span class="eror">{{ $message }}</span> @enderror
+                    </div>
+                </div>
             </x-slot>
             
             <x-slot name="footer">
