d0a1349fd530f0db2628eb7ce5f05ad235a85906
Michael p sianturi <michaelpsianturi@gmail.com>
2023-11-03 13:54:38 +0700

    update logo sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="fixed w-1/6 h-screen bg-teal-300">
     <div class="flex pt-8 ml-2">
         <a href="/dashboard">
-            <img src="/img/logo/ifsware-fullname.svg" alt="ifsware logo" width="100px" height="100px" class="w-16 h-16 ml-2">
+            <img src="/img/logo/ifsware-fullname.svg" alt="ifsware logo" width="100px" height="100px" class="h-16 ml-2 w-36">
         </a>
     </div>
     <div class="px-4 mt-10">
