107eca7ed44f556f664c6acb7f3c5e9ae49dcfc8
Michael p sianturi <michaelpsianturi@gmail.com>
2023-11-06 15:50:32 +0700

    count databases

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -3,11 +3,22 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\asset;
+use App\Models\datauser;
+use App\Models\accessories;
 
 class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard');
+        $assets = asset::count();
+        $datausers = datauser::count();
+        $Acceessories = accessories::count();
+
+        return view('livewire.dashboard', [
+            'assets' => $assets,
+            'datausers' => $datausers,
+            'Accessories' => $Acceessories
+        ]);
     }
 }
