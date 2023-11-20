45ef79832fb9026a06830d93c71a015076757d85
Michael p sianturi <michaelpsianturi@gmail.com>
2023-11-20 14:06:40 +0700

    make view histori activity

--- resources/views/components/table-list.blade.php
+++ resources/views/components/table-list.blade.php
@@ -22,19 +22,19 @@
                 </h1>
             </a>
         </li>
-            <li>
-                <a href="/listuser" class="flex p-2 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-teal-600 hover:bg-gray-50 group gap-x-3">
-                    <svg class="w-8 h-8 text-gray-400 shrink-0 group-hover:text-teal-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                    </svg>
-                    <h1 class="hidden lg:block">
-                        List User
-                    </h1>
-                    <h1 class="hidden lg:hidden md:block md:text-ellipsis md:overflow-hidden">
-                        ListUser
-                    </h1>
-                </a>
-            </li>
+        <li>
+            <a href="/listuser" class="flex p-2 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-teal-600 hover:bg-gray-50 group gap-x-3">
+                <svg class="w-8 h-8 text-gray-400 shrink-0 group-hover:text-teal-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                </svg>
+                <h1 class="hidden lg:block">
+                    List User
+                </h1>
+                <h1 class="hidden lg:hidden md:block md:text-ellipsis md:overflow-hidden">
+                    ListUser
+                </h1>
+            </a>
+        </li>
         <li>
             <a href="/accessories" class="flex p-2 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-teal-600 hover:bg-gray-50 group gap-x-3">
                 <x-accessorieslogo />
@@ -43,5 +43,13 @@
                 </h1>
             </a>
         </li>
-      </ul>
+        <li>
+            <a href="/histori_logs" class="flex p-2 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-teal-600 hover:bg-gray-50 group gap-x-3">
+                <x-accessorieslogo />
+                <h1 class="hidden md:block md:text-ellipsis md:overflow-hidden">
+                    Accessories
+                </h1>
+            </a>
+        </li>
+    </ul>
 </div>
