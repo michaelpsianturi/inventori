21a44c48bcf5b60e63f29291b3115e7b289ee91c
Michael p sianturi <michaelpsianturi@gmail.com>
2023-11-22 15:20:17 +0700

    Update table-list.blade.php

--- resources/views/components/table-list.blade.php
+++ resources/views/components/table-list.blade.php
@@ -45,7 +45,9 @@
         </li>
         <li>
             <a href="/histori_logs" class="flex p-2 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-teal-600 hover:bg-gray-50 group gap-x-3">
-                <x-accessorieslogo />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                  </svg>
                 <h1 class="hidden md:block md:text-ellipsis md:overflow-hidden">
                     Histori
                 </h1>
