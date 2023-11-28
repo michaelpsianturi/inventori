1374a5ed2d10652a0a347fc8cbb747c7784d96bc
Michael p sianturi <michaelpsianturi@gmail.com>
2023-11-28 13:03:06 +0700

    Update datauser.php

--- app/Models/datauser.php
+++ app/Models/datauser.php
@@ -20,7 +20,7 @@ class datauser extends Model
 
     public function assets()
     {
-        return $this->hasMany(asset::class);
+        return $this->belongsTo(asset::class);
     }
 
     public function Accessories()
