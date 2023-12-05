2e44de9c1923225906c9d01d0028b06da6dc8c26
Michael p sianturi <michaelpsianturi@gmail.com>
2023-12-05 12:16:51 +0700

    Update assetupdate.blade.php

--- resources/views/livewire/asset/assetupdate.blade.php
+++ resources/views/livewire/asset/assetupdate.blade.php
@@ -7,6 +7,15 @@
 
             <x-slot name="content">
                 <div>
+                    <h2 class="mt-4 mb-2 text-xl">User</h2>
+                    <select wire:model="selectedDatauserId">
+                        @foreach ($datausers as $datauser)
+                            <option value="{{ $datauser->id }}">{{ $datauser->profile_name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div>
                     <h2 class="mb-2 -mt-3 text-xl">Nama Barang</h2>
                     <input wire:model="product_name" class="block p-2 w-2/3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text">
                     <div>
@@ -41,15 +50,6 @@
                 <div>
                     <h2 class="mt-4 mb-2 text-xl">Description</h2>
                     <input wire:model="description" class="block w-2/3 p-2 text-gray-900 border-0 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text">
-                </div>
-
-                <div>
-                    <h2 class="mt-4 mb-2 text-xl">User</h2>
-                    <select wire:model="selectedDatauserId">
-                        @foreach ($datausers as $datauser)
-                            <option value="{{ $datauser->id }}">{{ $datauser->profile_name }}</option>
-                        @endforeach
-                    </select>
                 </div>
             </x-slot>
 
