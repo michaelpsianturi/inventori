c0ebf3393809dd91df3b241e94734f80e326495c
Michael P Sianturi <124184234+michaelpsianturi@users.noreply.github.com>
2024-01-19 12:32:36 +0700

    Update AssetSeeder.php

--- database/seeders/AssetSeeder.php
+++ database/seeders/AssetSeeder.php
@@ -21,12 +21,12 @@ class AssetSeeder extends Seeder
 
         for ($i = 0; $i < 10; $i++) {
             Asset::create([
-                'product_name' => $faker->randomElement($assetitem),
-                'product_price' => rand(10000, 100000),
-                'product_serial_number' => $faker->numberBetween(1000, 9999),
-                'product_stock' => rand(0, 100),
+                'products_name' => $faker->randomElement($assetitem),
+                'products_price' => rand(10000, 100000),
+                'products_serial_number' => $faker->numberBetween(1000, 9999),
+                'products_stock' => rand(0, 100),
                 'description' => $faker->text(100),
-                'datauser_id' => $faker->numberBetween(0, 99)
+                // 'datauser_id' => $faker->numberBetween(0, 99)
             ]);
         }
     }
