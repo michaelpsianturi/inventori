58725fd26a1c86f8f7b91b9907b76c643716e808
Michael P Sianturi <michaelpsianturi@gmail.com>
2025-04-11 14:50:27 +0700

    Add asset resources

---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,3 @@
+<a title="Home" href="/" class="inline-flex items-center gap-x-1.5">
+    <span class="text-black text-3xl/[0] font-semibold font-sans tracking-tighter" style="font-size: 22px">Inventory.</span>
+</a>
