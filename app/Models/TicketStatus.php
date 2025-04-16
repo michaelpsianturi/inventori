ba64a8f6397ca254183beea8092a38612480a087
Michael P Sianturi <michaelpsianturi@gmail.com>
2025-04-16 12:45:19 +0700

    Add ticket status models

---
+++ app/Models/TicketStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TicketStatus extends Model
+{
+    /** @use HasFactory<\Database\Factories\TicketStatusFactory> */
+    use HasFactory;
+}
