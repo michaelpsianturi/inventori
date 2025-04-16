d6ec82eec60ce4c652f91a3897ec45a4748beb9d
Michael P Sianturi <michaelpsianturi@gmail.com>
2025-04-16 12:14:43 +0700

    Add project model

---
+++ app/Models/Project.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProjectFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+}
